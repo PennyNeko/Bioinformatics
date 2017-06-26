741146ff6dd344558e1e3365dd85b64271dab25e
PennyNeko <penyamy@gmail.com>
2017-06-26 03:01:23 +0300

    implemented solution for problem 6.15

---
+++ 6.15.py
@@ -0,0 +1,27 @@
+selectedSeq = input("Select sequence to use (brain, liver or muscle)")
+seqFile = open("./6.12Seq/"+ selectedSeq+".txt", "r")
+seq = []
+lines = seqFile.readlines()[1:]
+for line in lines:
+    for letter in line:
+        seq.append(letter)
+canWin = []
+winningRemoval = []
+canWin.append(False)
+for i in range(1,len(seq)+1):
+    if not(canWin[i-1]):
+        canWin.append(True)
+        winningRemoval.append(1)
+    elif not(canWin[i-2]):
+        canWin.append(True)
+        winningRemoval.append(2)
+    else:
+        canWin.append(False)
+        winningRemoval.append(1)
+if (winningRemoval):
+    print("Player A won.")
+    print("Removing sequence was, starting with Player A:")
+    i = len(seq)-1
+    while(i>0):
+        print(winningRemoval[i])
+        i -= winningRemoval[i]
