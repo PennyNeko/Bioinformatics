d12713ec4c3ede8d6f87f6f4dde20f90edd692a8
PennyNeko <penyamy@gmail.com>
2017-06-26 03:11:34 +0300

    fixed sequence data for 6.15

---
+++ 6.12.py
@@ -0,0 +1,7 @@
+selectedSeq = input("Select sequence to use (brain, liver or muscle)")
+seqFile = open("./6.12Seq/"+ selectedSeq+".txt", "r")
+seq = []
+lines = seqFile.readlines()[1:]
+for line in lines:
+    for letter in line:
+        seq.append(letter)
